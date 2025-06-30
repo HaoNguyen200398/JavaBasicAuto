924228d744c0852a8fa66f39ea06862ed71d43f0
HaoNguyen200398 <haosongtu0@gmail.com>
2025-06-30 22:25:00 +0700

    update TC2 add line 3

--- src/TCHao2.java
+++ src/TCHao2.java
@@ -2,5 +2,6 @@ public class TCHao2 {
     public static void main(String[] args) {
         System.out.println("Login test 2");
         System.out.println("thich nhi");
+        System.out.println("update dong 3");
     }
 }
